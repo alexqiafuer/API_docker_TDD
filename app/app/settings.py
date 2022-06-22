6972be5dce3a8108b947909c0e018297e471c061
alexqiafuer <alexqiafuer@gmail.com>
2022-06-21 23:19:31 -0500

    Added Nginx proxy server configuration

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,13 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aj5c20-=erh0%6p)(k#xg(nopx8%q%@ex3h%38)robj@o)4m6z'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(',')
+    )
+)
 
 # Application definition
 
